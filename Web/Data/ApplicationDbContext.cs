aa9ececd92a17740cc68a787769958b0032085d6
md.karim <md.karim@summitcommunications.net>
2023-09-29 16:34:42 +0600

    identity added, login page design, admin panel created

--- Web/Data/ApplicationDbContext.cs
+++ Web/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Web.Models;
 
 namespace Web.Data
 {
@@ -9,5 +10,6 @@ namespace Web.Data
             : base(options)
         {
         }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
