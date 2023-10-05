9b5e05bdd1b2e08d700c773336fd6eb103b7a336
md.karim <md.karim@summitcommunications.net>
2023-10-05 19:52:22 +0600

    layout changes

---
+++ Web/Areas/DashBoard/Controllers/CampusController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Areas.DashBoard.Controllers
+{
+    [Area("DashBoard")]
+    public class CampusController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CampusController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> AboutOurs()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> History()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> Aims()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> InstitutionalStructure()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+
+        public async Task<DashBoardVM> GetDashBoardData()
+        {
+            var banner = await _context.Banner.ToListAsync();
+            var dashBoardModel = new DashBoardVM();
+            dashBoardModel.Banner1Src = banner[0].Path;
+            dashBoardModel.Banner2Src = banner[1].Path;
+            dashBoardModel.Banner3Src = banner[3].Path;
+            dashBoardModel.Banner4Src = banner[3].Path;
+            dashBoardModel.Banner5Src = banner[4].Path;
+            dashBoardModel.Banner6Src = banner[5].Path;
+            return dashBoardModel;
+        }
+    }
+}
