a9e200029be5af007f8cc8a9df17073791fcba6e
md.karim <md.karim@summitcommunications.net>
2023-10-07 15:11:26 +0600

    all controller added done

---
+++ Web/Areas/DashBoard/Controllers/AdmissionController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Areas.DashBoard.Controllers
+{
+    [Area("DashBoard")]
+    public class AdmissionController : Controller
+    {
+        private ApplicationDbContext _context;
+        public AdmissionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> HowToApply()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> Application()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> AdmissionTest()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> AdmissionRules()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> CurrentAdmissionStructure()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+
+        public async Task<DashBoardVM> GetDashBoardData()
+        {
+            var banner = await _context.Banner.ToListAsync();
+            var dashBoardModel = new DashBoardVM();
+            dashBoardModel.Banner1Src = banner[0].Path;
+            dashBoardModel.Banner2Src = banner[1].Path;
+            dashBoardModel.Banner3Src = banner[3].Path;
+            dashBoardModel.Banner4Src = banner[3].Path;
+            dashBoardModel.Banner5Src = banner[4].Path;
+            dashBoardModel.Banner6Src = banner[5].Path;
+            return dashBoardModel;
+        }
+    }
+}
---
+++ Web/Areas/DashBoard/Controllers/ResourceController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Areas.DashBoard.Controllers
+{
+    [Area("DashBoard")]
+    public class ResourceController : Controller
+    {
+        private ApplicationDbContext _context;
+        public ResourceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Library()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> ComputerLab()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> PlayGround()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> IctClub()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> Bncc()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> RedCrescent()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<IActionResult> RoverScoutt()
+        {
+            var dashBoardModel = await GetDashBoardData();
+            return View(dashBoardModel);
+        }
+        public async Task<DashBoardVM> GetDashBoardData()
+        {
+            var banner = await _context.Banner.ToListAsync();
+            var dashBoardModel = new DashBoardVM();
+            dashBoardModel.Banner1Src = banner[0].Path;
+            dashBoardModel.Banner2Src = banner[1].Path;
+            dashBoardModel.Banner3Src = banner[3].Path;
+            dashBoardModel.Banner4Src = banner[3].Path;
+            dashBoardModel.Banner5Src = banner[4].Path;
+            dashBoardModel.Banner6Src = banner[5].Path;
+            return dashBoardModel;
+        }
+    }
+}
